9d62d92e0b4d71962f1f892e9682d960c4f2ecd7
sharif-k <120332756+sharif-dotcom@users.noreply.github.com>
2024-04-15 18:24:33 +0300

    Add files via upload
    this another change that is just done

---
+++ website_form/website_form/website_form/doctype/webcitform/test_webcitform.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sharif kayore and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testwebcitform(FrappeTestCase):
+	pass
